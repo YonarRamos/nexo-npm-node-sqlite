7c8f5db9c72cc076dede41d25c2bf85226e529e0
IBLINETUM\yonar.ramos <yonar.ramos@inetum.com>
2023-02-20 12:31:39 +0100

    act a typescript

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,23 @@
 const knex = require('knex')
 
-let client;
+let client:any;
 
 let url = "";
-function setUrl(_url) {
+function setUrl(_url:any) {
     url = _url;
 }
 
-let onConnected;
-function setOnConnected(callback) {
+let onConnected:any;
+function setOnConnected(callback:any) {
     onConnected = callback;
 }
 
-let onFailure;
-function setOnFailure(callback) {
+let onFailure:any;
+function setOnFailure(callback:any) {
     onFailure = callback;
 };
 
-function initialize()
+function initialize(this: typeof client)
 {
     if (!url) throw new Error("url not defined");
 
--- src/tests/test.ts
+++ src/tests/test.ts
@@ -1,16 +1,16 @@
 
-const sqlite = require("../index.js")
+const sqlite = require("../index.ts")
 const logger = require('nexo-npm-node-logger')
 
 sqlite.url('./src/tests/test.sqlite3')
 sqlite.onConnected(async () => {
     logger.i("Connected to the database");
 });
-sqlite.onFailure((err) => {
+sqlite.onFailure((err:any) => {
     logger.e("Unable to connect to the database", err)
 });
 sqlite.initialize()
 sqlite.client('test_tb').select('*')
-.then((result)=> console.log(result))
-.catch(error =>  console.log(error))
+.then((result:any)=> console.log(result))
+.catch((error:any) =>  console.log(error))
 .finally(()=> process.exit())
